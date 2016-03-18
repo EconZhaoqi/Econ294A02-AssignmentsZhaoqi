54311c69b69f0927ef6182d0e6f9ddf9d13ee0e3
EconZhaoqi <zwang153@ucsc.edu>
2016-03-17 19:37:27 -0700

    Final new

--- Final of Econ 294-Zhaoqi Wang.R
+++ Final of Econ 294-Zhaoqi Wang.R
@@ -2,16 +2,13 @@
 ### Zhaoqi Wang ###
 ### 03/16/2016 ###
 
-library("knitr", lib.loc="/Library/Frameworks/R.framework/Versions/3.2/Resources/library")
-library("nycflights13", lib.loc="/Library/Frameworks/R.framework/Versions/3.2/Resources/library")
-library("dplyr", lib.loc="/Library/Frameworks/R.framework/Versions/3.2/Resources/library")
-library("RSQLite", lib.loc="/Library/Frameworks/R.framework/Versions/3.2/Resources/library")
-install.packages("RSQLite")
-flights
-summary(flights)
-weather
-my_db <- src_sqlite("my_db.sqlite", create = T)
-my_db
+library("dplyr")
+library("RSQLite")
+library("nycflights13")
+library("data.table")
+library("mfx")
+library("ggplot2")
+my_db <- nycflights13_sqlite()
 flights1<-flights%>% left_join(weather)
 flights2<-flights1 %>% left_join(airports, c("dest" = "faa"))
 flights3<-flights2%>%left_join(planes, by = "tailnum")
@@ -20,8 +17,6 @@ flights_sqlite <- copy_to(my_db, flights3, temporary = FALSE, indexes = list(
 flights = tbl(my_db, "flights3") %>% 
   collect() %>%
   mutate(canceled = is.na(arr_time))
-flights_sqlite <- copy_to(my_db, flights, temporary = FALSE, indexes = list(
-  c("year.x", "month", "day"), "carrier", "tailnum"))
 # The above code is trying to merge several different dataframs into one,
 # We have flights data, weather data, airports data and planes data.
 # By careating a data base my_db, we could put all those different datas into this base.
@@ -29,17 +24,17 @@ flights_sqlite <- copy_to(my_db, flights, temporary = FALSE, indexes = list(
 #####
 flightsnew <- dplyr::select(flights, year.x:day,dep_delay,dest,temp:visib,year.y:engine,canceled)
 # The above code is trying to select theo variables we need.
-
-install.packages("ggplot2")
-library(ggplot2)
-
-
 reg1<-lm(dep_delay~temp,flightsnew)                   
 summary(reg1)
 
-
-graph1<-qplot(temp,dep_delay, data = flightsnew,geom = "line",color = "red")
+graph1<-ggplot(
+  data = flightsnew,
+  aes(y=dep_delay,x = temp,size=wind_speed)
+)+
+  geom_point(aes(colour=visib), alpha=0.2)
 graph1
+
+
 # From the above regression result, we see that the coefficient of tempreture is 
 # just about 0.18, which is pretty small, even though it is statistically significant,
 # we can't conclude that tempreture will affect departure delay significantly.
@@ -59,8 +54,6 @@ summary(reg2)
 # relationship between dep_delay and wind_speed.
 
 ########################
-install.packages("data.table")
-library(data.table)
 outlierReplace = function(dataframe, cols, rows, newValue = NA) {
   if (any(rows)) {
     set(dataframe, rows, cols, newValue)
@@ -68,8 +61,12 @@ outlierReplace = function(dataframe, cols, rows, newValue = NA) {
 }
 outlierReplace(flightsnew, "wind_speed", which(flightsnew$wind_speed > 100), NA)
 
-qplot(wind_speed,dep_delay, data = flightsnew,geom = "line",color = "red")
-
+graph2<-ggplot(
+  data = flightsnew,
+  aes(y=dep_delay,x = wind_speed,size=wind_speed)
+)+
+  geom_point(aes(colour=visib), alpha=0.2)
+graph2
 # After we remove those outliers, the graph looks way better than the original one.
 # This time, compared with the one of tempreture, the variance of dep_delay is large,
 # meaning that wind_speed does affect dep_delay significaintly.
@@ -91,15 +88,16 @@ qplot(humid,dep_delay, data = flightsnew,geom = "line",color = "red")
 reg4<-lm(dep_delay~seats,flightsnew)                   
 summary(reg4)
 
-graph3<-qplot(seats,dep_delay, data = flightsnew,geom = "line",color = "red")
-
+graph3<-ggplot(
+  data = flightsnew,
+  aes(y=dep_delay,x = seats)
+)+
+  geom_point(aes(colour=visib), alpha=0.2)
+graph3
 # Seats, as we could see from the regression result and the graph, won't affect dep_delay 
 # that much. 
 
 ##################
-install.packages("mfx")
-library(mfx)
-
 probitmfx(canceled~temp+dewp+humid+wind_speed+visib+engines+seats,data=flightsnew,robust=T)
 
 # Also we are interested in the relationship among canceled flights and all those variables.
@@ -107,9 +105,6 @@ probitmfx(canceled~temp+dewp+humid+wind_speed+visib+engines+seats,data=flightsne
 # We install mfx package and run the regression above.
 # From the result, we see that dewp, wind_speed and engines affect canelation very much.
 # They are all significant and the coefficients are all pretty large.
-
-
-
 
 
 
